6d3750840c9e9979e521163b068fe8a04da3cee2
Mateusz Składanek <s23629@pjwstk.edu.pl>
2022-06-14 08:51:41 +0200

    dodano klasy

---
+++ Models/Track.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace s23629.Models
+{
+    public class Track
+    {
+        public int IdTrack { get; set; }
+        public string TrackName { get; set; }
+
+        public float Duration { get; set; }
+
+        public int? IdMusicAlbum { get; set; }
+
+        public virtual Album Album { get; set; }
+        public virtual ICollection<MusicianTrack> MusicianTracks { get; set; }
+    }
+}
